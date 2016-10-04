7151cb83fac44d5be4ae22e055419f33829c9897
jcocking <jeff.cocking@gmail.com>
2016-10-04 18:32:37 -0400

    file clean-up

--- language/cs/fancybox_acp.php
+++ language/cs/fancybox_acp.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *
- * Fancybox extension for the phpBB Forum Software package.
- *
- * @copyright (c) 2015 Jeff Cocking
- * @license GNU General Public License, version 2 (GPL-2.0)
- *
- */
+*
+* @package phpBB Extension - Social Share
+* @copyright (c) 2016 Jeff Cocking
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
 /**
  * DO NOT CHANGE
  */
@@ -14,11 +14,14 @@ if (!defined('IN_PHPBB'))
 {
 	exit;
 }
+
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
+
 $lang = array_merge($lang, array(
+	'LOTUSJEFF_FANCYBOX_TITLE'				=> 'FancyBox - A Lightbox Alternative',
 	'LOTUSJEFF_FANCYBOX_SETTINGS'			=> 'Nastavení rozšíření Fancybox',
 	'LOTUSJEFF_FANCYBOX_BUTTON'				=> 'Rozšířený ovládací panel',
 	'LOTUSJEFF_FANCYBOX_BUTTON_EXPLAIN'		=> 'Rozšířený ovládací panel se zobrazuje nad obrázky ve Fancybox prohlížeči. Umožňuje spuštění automatické prezentace.',
