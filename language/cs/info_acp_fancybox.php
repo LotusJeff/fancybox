0efc887abc6d150eb69dfdfb897fc6f8371eeaaf
R3gi <regiprogi@gmail.com>
2016-10-05 20:00:19 +0200

    Czech translation
    Czech translation

---
+++ language/cs/info_acp_fancybox.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* @package phpBB Extension - Fancybox
+* @copyright (c) 2016 Jeff Cocking
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	// ACP Module
+	'ACP_FANCYBOX_TITLE'			=> 'Fancybox',
+	'ACP_FANCYBOX_SETTINGS'		=> 'Nastavení',
+));
