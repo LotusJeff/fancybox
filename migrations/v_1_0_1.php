55df837a5a20e58d9e4b3e44f309b47f150d47a6
jcocking <jeff.cocking@gmail.com>
2016-10-11 09:45:26 -0400

    version 1.0.1

---
+++ migrations/v_1_0_1.php
@@ -0,0 +1,35 @@
+<?php
+/**
+*
+* @package phpBB Extension - Fancybox
+* @copyright (c) 2016 Jeff Cocking
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace lotusjeff\fancybox\migrations;
+
+class v_1_0_1 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['lotusjeff_fancybox_versions']) && version_compare($this->config['lotusjeff_fancybox_versions'], '1.0.1', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\lotusjeff\fancybox\migrations\v_1_0_0');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.add', array('lotusjeff_fancybox_varsions', '1.0.1')),
+			array('module.add', array(
+				'acp',
+				'ACP_FANCYBOX_TITLE',
+				),
+			)),
+		);
+	}
+}
